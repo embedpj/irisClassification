2e8b1ab61ba3a2212e7db44885924ac2483b140d
embedpj <prabhujyoti@hotmail.com>
2021-07-10 20:38:44 +0800

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,14 @@
+timeout 0
+rem pip3 install torchvision
+rem pip3 install tqdm
+rem pip install ipywidgets
+rem jupyter nbextension enable --py widgetsnbextension
+rem pip install matplotlib
+rem pip install pandas
+rem pip install seaborn
+rem pip install sklearn
+rem pip install pipreqsnb
+rem "D:\JPY\python 3\"python app.py
+"D:\JPY\python 3\Scripts\pipreqsnb" --force "D:\JPY\python 3\Scripts\mynotebook\dataset\irisclassification"
+rem "D:\JPY\python 3\Scripts\"pipreqsnb "D:/JPY/python\ 3\/Scripts/mynotebook/dataset/irisclassification"
+timeout 30
